5a317d385b9902283324ccbe1e986fc2acaea0b4
Yegor Ivashchenko <yegor.ivashchenko@protonmail.com>
2025-01-25 13:06:53 +0000

    Add second page

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold">Rehearsal scheduler</h1>
-            <p className="text-lg mt-4">This site is designed for mobile devices.</p>
-            <p className="text-lg">Please use a mobile device to access the site.</p>
+            <h1 className="text-4xl font-bold">Choose date</h1>
+            {/*<p className="text-lg mt-4">This site is designed for mobile devices.</p>*/}
+            {/*<p className="text-lg">Please use a mobile device to access the site.</p>*/}
             {/* when button is clicked, navigate to /schedule */}
-            <p className="text-lg mt-4">To continue, click the button below.</p>
+            {/*<p className="text-lg mt-4">To continue, click the button below.</p>*/}
             {/*<button className="mt-8 px-4 py-2 bg-blue-500 text-white rounded-md">Start!</button>*/}
-            <Link href={`/schedule`}>Schedule!</Link>
+            <Link href={`/`}>Home!</Link>
         </div>
     );
 }
